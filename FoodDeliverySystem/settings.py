a90d20272dde95f76673ab0c69e294dc2666d115
YucelAl <s324612@uos.ac.uk>
2026-03-12 11:05:32 +0000

    Commented version

--- FoodDeliverySystem/settings.py
+++ FoodDeliverySystem/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-omehgbs%l4g6j+vpaax^l8vf@hxdw7+#&dph6gewfhj@i)eh0g
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -117,3 +118,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
